2084108708edbfd61450aa7448f9d1745cc2fb70
Phefu Madyo <phefumadyo@gmail.com>
2024-09-16 12:31:42 +0300

    Extracted information from Weather.js file to WeatherInfo.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,18 @@
 import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
- const [weatherData, setWeatherData] = useState({ready:false})
+ const [weatherData, setWeatherData] = useState({ready:false});
+ const [city, setCity] = useState(props.defaultCity);
+ 
  function handleResponse(response){
   console.log(response);
   setWeatherData({
    ready: true,
    temperature: response.data.temperature.current,
-   date: new Date(response.data.date * 1000),
+   date: new Date(response.data.time * 1000),
    description: response.data.condition.description,
    icon_url: response.data.condition.icon_url,
    humidity: response.data.temperature.humidity,
@@ -18,17 +20,22 @@ export default function Weather(props) {
    city: response.data.name,
   });
  }
- 
+
+function handleCityChange(event) {
+    setCity(event.target.value);
+}
 
  function handleSubmit(event){
   event.preventDefault();
+  search()
  }
 
- function searchCity(city) {
+ function search() {
   const apiKey= "4o05t4b450f7bf3040828beac3b1db1c";
-  let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`
+  let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
   axios.get(apiUrl).then(handleResponse);
  }
+ 
  
 
  if (weatherData.ready) {
@@ -42,6 +49,7 @@ export default function Weather(props) {
                     placeholder="Enter a city.."
                     className="form-control"
                     autoFocus="on"
+                    onChange={handleCityChange}
                     />
                     </div>
                         <div className="col-3">
@@ -49,46 +57,14 @@ export default function Weather(props) {
                         </div>
                       </div>
                     </form>
-                    <br />
-                    <h1>{weatherData.city}</h1>
-                    <ul>
-                        <li>
-                          <FormattedDate date={weatherData.date} />
-                        </li>
-                        <li  className="text-capitalization">
-                         {weatherData.description}
-                        </li>
-                    </ul>
-
-                    <div className="row mt-3">
-                        <div className="col-6">
-                         
-                            <img src= {weatherData.icon_url}
-                            alt= {weatherData.description}
-                           
-                            />
-                            
-                            
-                            <span className="temperature">
-                             {Math.round(weatherData.temperature)}
-                            </span>
-                            <span className="units">°C</span>
-                           
-                            
-                            </div>
-                            <div className="col-6">
-                                <ul>
-                                    <li>Humidity:{weatherData.humidity} %</li>
-                                    <li>Wind: {weatherData.wind} 13km/h</li>
-                                </ul>
-                            </div>
-                          </div>
+                    <WeatherInfo data= {weatherData} />
+                    
+                    
                         </div>
                       
                         ); 
                        } else{
-                        searchCity(props.defaultCity);
-                        
+                        search();
                         return "Loading..";
                        }
                       
---
+++ src/WeatherInfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props){
+    return (
+        <div className="WeatherInfo">
+          <h1>{props.data.city}</h1>
+                    <ul>
+                        <li>
+                          <FormattedDate date={props.data.date} />
+                        </li>
+                        <li  className="text-capitalization">
+                         {props.data.description}
+                        </li>
+                    </ul>
+
+                    <div className="row mt-3">
+                        <div className="col-6">
+                         
+                            <img src= {props.data.icon_url}
+                            alt= {props.data.description}
+                           
+                            />
+                            <span className="temperature">
+                             {Math.round(props.data.temperature)}
+                            </span>
+                            <span className="units">°C</span>
+                           
+                            
+                            </div>
+                            <div className="col-6">
+                                <ul>
+                                    <li>Humidity:{props.data.humidity} %</li>
+                                    <li>Wind: {props.data.wind} km/h</li>
+                                </ul>
+                            </div>
+                          </div>
+                        </div>
+);
+}
